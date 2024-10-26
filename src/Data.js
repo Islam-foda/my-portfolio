865dd8b83a9f8ee636025e27518314b0228dc0bd
islam-foda <eslam.foda@hotmail.com>
2024-10-26 14:09:10 +0300

    fix broken links

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ export const projects = [
     id: 1,
     projectLink: "https://generate-rnd-meme.netlify.app/",
     githubLink: "https://github.com/Islam-foda/Meme-Generator",
-    img: "./src/assets/meme-generator.png",
+    img: "https://github.com/user-attachments/assets/d2365f08-188b-4e0c-a158-a1711af80914",
     technologies: ["react", "css", "js"],
     description:
       "A meme generator app that allows users to generate memes with various templates and styles.",
@@ -22,8 +22,8 @@ export const projects = [
     title: "Tours App",
     id: 3,
     projectLink: "https://tours-prefered.netlify.app/",
-    githubLink: "https://github.com/Islam-foda/Template-Two",
-    img: "./src/assets/tours.png ",
+    githubLink: "https://github.com/Islam-foda/Tours",
+    img: "https://github.com/user-attachments/assets/ccd31ccd-e4b5-4edf-905e-e60d98872479 ",
     technologies: ["React", "css", "js"],
     description:
       "A Tours App that allows users to view different tours and book them.",
